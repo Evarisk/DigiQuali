0b70babf3300eeda4f7f69a3f6690592b4da3bf6
Nicolas <nicolas@eoxia.com>
2022-01-06 18:17:59 +0100

    [Sheet] add: lib

--- lib/dolismq.lib.php
+++ lib/dolismq.lib.php
@@ -57,6 +57,11 @@ function dolismqAdminPrepareHead()
 	$head[$h][2] = 'question';
 	$h++;
 
+	$head[$h][0] = dol_buildpath("/dolismq/admin/sheet.php", 1);
+	$head[$h][1] = $langs->trans("Sheet");
+	$head[$h][2] = 'sheet';
+	$h++;
+
 	// Show more tabs from modules
 	// Entries must be declared in modules descriptor with line
 	//$this->tabs = array(
