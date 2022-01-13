443ee0cee36281f03814639e2568c23befd5e5cf
Nicolas <nicolas@eoxia.com>
2022-01-13 10:39:11 +0100

    [Sheet] add: events page

--- lib/dolismq_sheet.lib.php
+++ lib/dolismq_sheet.lib.php
@@ -1,9 +1,9 @@
 <?php
-/* Copyright (C) ---Put here your own copyright and developer email---
+/* Copyright (C) 2021 EOXIA <dev@eoxia.com>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,19 +12,20 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 /**
  * \file    lib/dolismq_sheet.lib.php
  * \ingroup dolismq
- * \brief   Library files with common functions for Question
+ * \brief   Library files with common functions for Sheet
  */
 
 /**
- * Prepare array of tabs for Question
+ * Prepare array of tabs for Sheet
  *
- * @param	Question	$object		Question
+ * @param	Sheet	$object		    Sheet
  * @return 	array					Array of tabs
  */
 function sheetPrepareHead($object)
@@ -36,47 +37,27 @@ function sheetPrepareHead($object)
 	$h = 0;
 	$head = array();
 
-	$head[$h][0] = dol_buildpath("/dolismq/sheet_card.php", 1).'?id='.$object->id;
+	$head[$h][0] = dol_buildpath("/dolismq/view/sheet/sheet_card.php", 1).'?id='.$object->id;
 	$head[$h][1] = $langs->trans("Card");
 	$head[$h][2] = 'card';
 	$h++;
-
-	if (isset($object->fields['note_public']) || isset($object->fields['note_private']))
-	{
-		$nbNote = 0;
-		if (!empty($object->note_private)) $nbNote++;
-		if (!empty($object->note_public)) $nbNote++;
-		$head[$h][0] = dol_buildpath('/dolismq/sheet_note.php', 1).'?id='.$object->id;
-		$head[$h][1] = $langs->trans('Notes');
-		if ($nbNote > 0) $head[$h][1] .= (empty($conf->global->MAIN_OPTIMIZEFORTEXTBROWSER) ? '<span class="badge marginleftonlyshort">'.$nbNote.'</span>' : '');
-		$head[$h][2] = 'note';
-		$h++;
-	}
 
 	require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 	require_once DOL_DOCUMENT_ROOT.'/core/class/link.class.php';
 	$upload_dir = $conf->dolismq->dir_output."/sheet/".dol_sanitizeFileName($object->ref);
 	$nbFiles = count(dol_dir_list($upload_dir, 'files', 0, '', '(\.meta|_preview.*\.png)$'));
 	$nbLinks = Link::count($db, $object->element, $object->id);
-	$head[$h][0] = dol_buildpath("/dolismq/sheet_document.php", 1).'?id='.$object->id;
+	$head[$h][0] = dol_buildpath("/dolismq/view/sheet/sheet_document.php", 1).'?id='.$object->id;
 	$head[$h][1] = $langs->trans('Documents');
 	if (($nbFiles + $nbLinks) > 0) $head[$h][1] .= '<span class="badge marginleftonlyshort">'.($nbFiles + $nbLinks).'</span>';
 	$head[$h][2] = 'document';
 	$h++;
 
-	$head[$h][0] = dol_buildpath("/dolismq/sheet_agenda.php", 1).'?id='.$object->id;
+	$head[$h][0] = dol_buildpath("/dolismq/view/sheet/sheet_agenda.php", 1).'?id='.$object->id;
 	$head[$h][1] = $langs->trans("Events");
 	$head[$h][2] = 'agenda';
 	$h++;
 
-	// Show more tabs from modules
-	// Entries must be declared in modules descriptor with line
-	//$this->tabs = array(
-	//	'entity:+tabname:Title:@dolismq:/dolismq/mypage.php?id=__ID__'
-	//); // to add new tab
-	//$this->tabs = array(
-	//	'entity:-tabname:Title:@dolismq:/dolismq/mypage.php?id=__ID__'
-	//); // to remove a tab
 	complete_head_from_modules($conf, $langs, $object, $head, $h, 'sheet@dolismq');
 
 	complete_head_from_modules($conf, $langs, $object, $head, $h, 'sheet@dolismq', 'remove');
