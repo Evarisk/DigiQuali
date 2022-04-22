7d816754464f6dfc943e0bf3de465d7843ea9022
Nicolas <nicolas@eoxia.com>
2022-04-22 11:34:47 +0200

    [SQL] add: update sql for new field control

--- sql/update.sql
+++ sql/update.sql
@@ -1,4 +1,12 @@
 -- 1.1.0
 ALTER TABLE `llx_dolismq_controldet` ADD `answer_photo` TEXT NOT NULL AFTER `answer`;
 
+-- 1.2.0
+ALTER TABLE `llx_dolismq_control` ADD `note_public` TEXT NULL AFTER `status`;
+ALTER TABLE `llx_dolismq_control` ADD `note_private` TEXT NULL AFTER `note_public`;
+ALTER TABLE `llx_dolismq_control` DROP `fk_product`;
+ALTER TABLE `llx_dolismq_control` DROP `fk_lot`;
+ALTER TABLE `llx_dolismq_control` DROP `fk_soc`;
+ALTER TABLE `llx_dolismq_control` DROP `fk_project`;
+ALTER TABLE `llx_dolismq_control` DROP `fk_task`;
 
