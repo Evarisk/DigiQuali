9890b290382043664f194b41342f1c2a3203e17d
Nicolas Domenech <nicolas@eoxia.com>
2022-06-15 11:52:38 +0200

    [Sheet] fix: remove backtopage in sheet list add button

--- view/sheet/sheet_list.php
+++ view/sheet/sheet_list.php
@@ -358,7 +358,7 @@ print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
 print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
 print '<input type="hidden" name="contextpage" value="'.$contextpage.'">';
 
-$newcardbutton = dolGetButtonTitle($langs->trans('NewSheet'), '', 'fa fa-plus-circle', dol_buildpath('/dolismq/view/sheet/sheet_card.php', 1).'?action=create&backtopage='.urlencode($_SERVER['PHP_SELF']), '', $permissiontoadd);
+$newcardbutton = dolGetButtonTitle($langs->trans('NewSheet'), '', 'fa fa-plus-circle', dol_buildpath('/dolismq/view/sheet/sheet_card.php', 1).'?action=create', '', $permissiontoadd);
 
 print_barre_liste($title, $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, $massactionbutton, $num, $nbtotalofrecords, "dolismq@dolismq", 0, $newcardbutton, '', $limit, 0, 0, 1);
 
