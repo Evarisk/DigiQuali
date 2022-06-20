397d5cdb847c0fd9a3275f589da840045acffa1f
Nicolas Domenech <nicolas@eoxia.com>
2022-06-20 17:50:54 +0200

    [Question] add: title question in edit action

--- view/question/question_card.php
+++ view/question/question_card.php
@@ -561,7 +561,7 @@ include DOL_DOCUMENT_ROOT . '/custom/dolismq/core/tpl/dolismq_medias_gallery_mod
 
 // Part to edit record
 if (($id || $ref) && $action == 'edit') {
-	print load_fiche_titre($langs->trans("Question"), '', 'object_'.$object->picto);
+	print load_fiche_titre($langs->trans("ModifyQuestion"), '', 'dolismq@dolismq');
 
 	print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
 	print '<input type="hidden" name="token" value="'.newToken().'">';
