77ccbfc58c385522057c1c1d00da7bf0113e7ca5
Nicolas Domenech <nicolas@eoxia.com>
2022-11-17 10:05:55 +0100

    #499 [Object] add: clean agenda view files

--- view/sheet/sheet_agenda.php
+++ view/sheet/sheet_agenda.php
@@ -22,91 +22,93 @@
  */
 
 // Load Dolibarr environment
-$res = 0;
-// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if ( ! $res && ! empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php";
-// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
-$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
-while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) { $i--; $j--; }
-if ( ! $res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res          = @include substr($tmp, 0, ($i + 1)) . "/main.inc.php";
-if ( ! $res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php")) $res = @include dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php";
-// Try main.inc.php using relative path
-if ( ! $res && file_exists("../../main.inc.php")) $res       = @include "../../main.inc.php";
-if ( ! $res && file_exists("../../../main.inc.php")) $res    = @include "../../../main.inc.php";
-if ( ! $res && file_exists("../../../../main.inc.php")) $res = @include "../../../../main.inc.php";
-if ( ! $res) die("Include of main fails");
+if (file_exists('../../../../main.inc.php')) {
+	require_once '../../../../main.inc.php';
+} else {
+	die('Include of main fails');
+}
 
 // Libraries
-require_once DOL_DOCUMENT_ROOT . '/contact/class/contact.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/company.lib.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
 
 require_once __DIR__ . '/../../class/sheet.class.php';
 require_once __DIR__ . '/../../lib/dolismq_sheet.lib.php';
-require_once __DIR__ . '/../../lib/dolismq_function.lib.php';
 
 // Global variables definitions
 global $conf, $db, $langs, $hookmanager, $user;
 
 // Load translation files required by the page
-$langs->loadLangs(array("dolismq@dolismq", "other"));
+$langs->loadLangs(['dolismq@dolismq', 'other']);
 
 // Get parameters
 $id         = GETPOST('id', 'int');
 $ref        = GETPOST('ref', 'alpha');
 $action     = GETPOST('action', 'alpha');
-$cancel     = GETPOST('cancel', 'aZ09');
 $backtopage = GETPOST('backtopage', 'alpha');
 
 if (GETPOST('actioncode', 'array')) {
-	$actioncode                            = GETPOST('actioncode', 'array', 3);
-	if ( ! count($actioncode)) $actioncode = '0';
+	$actioncode = GETPOST('actioncode', 'array', 3);
+	if (!count($actioncode)) {
+		$actioncode = '0';
+	}
 } else {
-	$actioncode = GETPOST("actioncode", "alpha", 3) ? GETPOST("actioncode", "alpha", 3) : (GETPOST("actioncode") == '0' ? '0' : (empty($conf->global->AGENDA_DEFAULT_FILTER_TYPE_FOR_OBJECT) ? '' : $conf->global->AGENDA_DEFAULT_FILTER_TYPE_FOR_OBJECT));
+	$actioncode = GETPOST('actioncode', 'alpha', 3) ? GETPOST('actioncode', 'alpha', 3) : (GETPOST('actioncode') == '0' ? '0' : (empty($conf->global->AGENDA_DEFAULT_FILTER_TYPE_FOR_OBJECT) ? '' : $conf->global->AGENDA_DEFAULT_FILTER_TYPE_FOR_OBJECT));
 }
+
 $search_agenda_label = GETPOST('search_agenda_label');
 
 $limit     = GETPOST('limit', 'int') ? GETPOST('limit', 'int') : $conf->liste_limit;
-$sortfield = GETPOST("sortfield", 'alpha');
-$sortorder = GETPOST("sortorder", 'alpha');
-$page      = GETPOSTISSET('pageplusone') ? (GETPOST('pageplusone') - 1) : GETPOST("page", 'int');
-if (empty($page) || $page == -1) { $page = 0; }     // If $page is not defined, or '' or -1
-$offset                       = $limit * $page;
-$pageprev                     = $page - 1;
-$pagenext                     = $page + 1;
-if ( ! $sortfield) $sortfield = 'a.datep,a.id';
-if ( ! $sortorder) $sortorder = 'DESC,DESC';
+$sortfield = GETPOST('sortfield', 'alpha');
+$sortorder = GETPOST('sortorder', 'alpha');
+$page      = GETPOSTISSET('pageplusone') ? (GETPOST('pageplusone') - 1) : GETPOST('page', 'int');
+if (empty($page) || $page == -1) { // If $page is not defined, or '' or -1
+	$page = 0;
+}
+$offset   = $limit * $page;
+$pageprev = $page - 1;
+$pagenext = $page + 1;
+if (!$sortfield) {
+	$sortfield = 'a.datep,a.id';
+}
+if (!$sortorder) {
+	$sortorder = 'DESC,DESC';
+}
 
 // Initialize technical objects
 $object      = new Sheet($db);
 $extrafields = new ExtraFields($db);
 
-$hookmanager->initHooks(array('sheetagenda', 'globalcard')); // Note that conf->hooks_modules contains array
+$hookmanager->initHooks(['sheetagenda', 'globalcard']); // Note that conf->hooks_modules contains array
 
 // Fetch optionals attributes and labels
 $extrafields->fetch_name_optionals_label($object->table_element);
 
 // Load object
-include DOL_DOCUMENT_ROOT . '/core/actions_fetchobject.inc.php'; // Must be include, not include_once  // Must be include, not include_once. Include fetch and fetch_thirdparty but not fetch_optionals
-if ($id > 0 || ! empty($ref)) $upload_dir = $conf->dolismq->multidir_output[$object->entity] . "/" . $object->id;
+include DOL_DOCUMENT_ROOT . '/core/actions_fetchobject.inc.php'; // Must be included, not include_once. Include fetch and fetch_thirdparty but not fetch_optionals
+if ($id > 0 || ! empty($ref)) {
+	$upload_dir = $conf->dolismq->multidir_output[$object->entity] . '/' . $object->id;
+}
 
 // Security check - Protection if external user
 $permissiontoread = $user->rights->dolismq->sheet->read;
-
-if ( ! $permissiontoread) accessforbidden();
+if (empty($conf->dolismq->enabled)) accessforbidden();
+if (!$permissiontoread) accessforbidden();
 
 /*
  *  Actions
  */
 
-$parameters = array('id' => $id);
+$parameters = ['id' => $id];
 $reshook    = $hookmanager->executeHooks('doActions', $parameters, $object, $action); // Note that $action and $object may have been modified by some hooks
-if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+if ($reshook < 0) {
+	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+}
 
 if (empty($reshook)) {
 	// Cancel
 	if (GETPOST('cancel', 'alpha') && ! empty($backtopage)) {
-		header("Location: " . $backtopage);
+		header('Location: ' . $backtopage);
 		exit;
 	}
 
@@ -122,12 +124,12 @@ if (empty($reshook)) {
  */
 
 if ($object->id > 0) {
-	$title    = $langs->trans("Sheet") . ' - ' . $langs->trans("Agenda");
+	$title    = $langs->trans('Sheet') . ' - ' . $langs->trans('Agenda');
 	$help_url = 'FR:Module_DoliSMQ';
-	$morejs   = array("/dolismq/js/dolismq.js");
-	$morecss  = array("/dolismq/css/dolismq.css");
+	$morejs   = ['/dolismq/js/dolismq.js'];
+	$morecss  = ['/dolismq/css/dolismq.css'];
 
-	llxHeader('', $title, $help_url, '', '', '', $morejs, $morecss);
+	llxHeader('', $title, $help_url, '', 0, 0, $morejs, $morecss);
 
 	$head = sheetPrepareHead($object);
 
@@ -135,8 +137,10 @@ if ($object->id > 0) {
 
 	// Object card
 	// ------------------------------------------------------------
-	dol_strlen($object->label) ? $morehtmlref = ' - ' . $object->label : '';
-	dol_banner_tab($object, 'ref', '', 0, 'ref', 'ref', $morehtmlref);
+	$linkback = '<a href="'.dol_buildpath('/dolisirh/view/sheet/sheet_list.php', 1).'">'.$langs->trans('BackToList').'</a>';
+
+	$object->picto = 'sheet_small@dolisirh';
+	dol_banner_tab($object, 'ref', $linkback, 1, 'ref');
 
 	print '<div class="fichecenter">';
 	print '<div class="underbanner clearboth"></div>';
@@ -149,24 +153,36 @@ if ($object->id > 0) {
 	print dol_get_fiche_end();
 
 	// Actions buttons
-	$out = '&origin=' . $object->element . '@dolismq' . '&originid=' . $object->id . '&backtopage=1&percentage=-1';
+	$out = '&origin=' . urlencode($object->element.'@'.$object->module) . '&originid=' . urlencode($object->id);
+	$urlbacktopage = $_SERVER['PHP_SELF'] . '?id=' . $object->id;
+	$out .= '&backtopage='.urlencode($urlbacktopage);
 
-	if ( ! empty($conf->agenda->enabled)) {
-		$linktocreatetimeBtnStatus = ! empty($user->rights->agenda->myactions->create) || ! empty($user->rights->agenda->allactions->create);
-		$morehtmlcenter            = dolGetButtonTitle($langs->trans('AddAction'), '', 'fa fa-plus-circle', DOL_URL_ROOT . '/comm/action/card.php?action=create' . $out, '', $linktocreatetimeBtnStatus);
+	print '<div class="tabsAction">';
+	if (!empty($conf->agenda->enabled)) {
+		if (!empty($user->rights->agenda->myactions->create) || !empty($user->rights->agenda->allactions->create)) {
+			print '<a class="butAction" href="' . DOL_URL_ROOT . '/comm/action/card.php?action=create' . $out . '">' . $langs->trans('AddAction') . '</a>';
+		} else {
+			print '<span class="butActionRefused classfortooltip" title="' . dol_escape_htmltag($langs->trans('PermissionDenied')) . '">'. $langs->trans('AddAction') . '</span>';
+		}
 	}
+	print '</div>';
 
-	if ( ! empty($conf->agenda->enabled) && ( ! empty($user->rights->agenda->myactions->read) || ! empty($user->rights->agenda->allactions->read))) {
-		$param                                                                      = '&id=' . $object->id;
-		if ( ! empty($contextpage) && $contextpage != $_SERVER["PHP_SELF"]) $param .= '&contextpage=' . urlencode($contextpage);
-		if ($limit > 0 && $limit != $conf->liste_limit) $param                     .= '&limit=' . urlencode($limit);
+	if (!empty($conf->agenda->enabled) && (!empty($user->rights->agenda->myactions->read) || !empty($user->rights->agenda->allactions->read))) {
+		$param = '&id=' . $object->id;
+		if (!empty($contextpage) && $contextpage != $_SERVER['PHP_SELF']) {
+			$param .= '&contextpage=' . urlencode($contextpage);
+		}
+		if ($limit > 0 && $limit != $conf->liste_limit) {
+			$param .= '&limit=' . urlencode($limit);
+		}
 
-		print_barre_liste($langs->trans("ActionsOnSheet"), 0, $_SERVER["PHP_SELF"], '', $sortfield, $sortorder, '', 0, -1, '', 0, $morehtmlcenter, '', 0, 1, 1);
+		print load_fiche_titre($langs->trans('ActionsOnSheet'), '', '');
 
 		// List of all actions
-		$filters = array();
-		show_actions_done($conf, $langs, $db, $object, null, 0, $actioncode, '', $filters, $sortfield, $sortorder, 'dolismq');
-		print '</div>';
+		$filters = [];
+		$filters['search_agenda_label'] = $search_agenda_label;
+
+		show_actions_done($conf, $langs, $db, $object, null, 0, $actioncode, '', $filters, $sortfield, $sortorder, $object->module);
 	}
 }
 
