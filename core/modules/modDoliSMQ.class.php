850694ed157be510146f80503442c14905c57ec6
Théo David <theo.david@evarisk.com>
2023-02-09 15:09:41 +0100

    #620 [Menu] fix: remove module config menu entry

--- core/modules/modDoliSMQ.class.php
+++ core/modules/modDoliSMQ.class.php
@@ -469,22 +469,6 @@ class modDoliSMQ extends DolibarrModules
 			'target'   => '',
 			'user'     => 0,
 		];
-
-		$this->menu[$r++] = [
-			'fk_menu'  => 'fk_mainmenu=dolismq',												// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-			'type'     => 'left',																// This is a Left menu entry
-			'titre'    => $langs->trans('DoliSMQConfig'),
-			'prefix'   => '<i class="fas fa-cog pictofixedwidth"></i>',
-			'mainmenu' => 'dolismq',
-			'leftmenu' => 'dolismqconfig',
-			'url'      => '/dolismq/admin/setup.php',
-			'langs'    => 'dolismq@dolismq',													// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-			'position' => 1000 + $r,
-			'enabled'  => '$conf->dolismq->enabled',  											// Define condition to show or hide menu entry. Use '$conf->dolismq->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-			'perms'    => '$user->rights->dolismq->adminpage->read',							// Use 'perms'=>'$user->rights->dolismq->level1->level2' if you want your menu with a permission rules
-			'target'   => '',
-			'user'     => 0,																	// 0=Menu for internal users, 1=external users, 2=both
-		];
 	}
 
 	/**
