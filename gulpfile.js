503280cf52f55d7c5d8f0dbabe6b70292fc8905a
Théo David <theo.david@evarisk.com>
2023-02-17 17:03:36 +0100

    #687 [JS/CSS] fix: gulpfile & minimized file creation

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,17 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass')(require('sass'));
 var rename       = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
 var uglify       = require('gulp-uglify');
 var concat       = require('gulp-concat');
 
 var paths = {
 	scss_core : [ 'css/scss/**/*.scss', 'css/' ],
-	js_backend: [ 'js/constructor.js', 'js/modules/*.js', 'js/' ]
+	js_backend: [ 'js/dolismq.js', 'js/modules/*.js' ]
 };
 
 /** Core */
 gulp.task( 'scss_core', function() {
 	return gulp.src( paths.scss_core[0] )
-		.pipe(sass({'outputStyle': 'expanded'}).on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}) )
-		.pipe(rename('./dolismq.css'))
-		.pipe(gulp.dest(paths.scss_core[1]))
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(rename('./dolismq.min.css'))
 		.pipe(gulp.dest( paths.scss_core[1]));
@@ -30,8 +22,6 @@ gulp.task( 'scss_core', function() {
 
 gulp.task('js_backend', function () {
 	return gulp.src(paths.js_backend)
-		.pipe(concat('dolismq.js'))
-		.pipe(gulp.dest('./js/'))
 		.pipe(concat('dolismq.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./js/'))
