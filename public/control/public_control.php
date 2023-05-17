82be8aeba412f0d1ceccbc33cc3657a494b90dda
Nicolas Domenech <nicolas@eoxia.com>
2023-05-17 10:16:51 +0200

    #643 [Public] add: improve CSS and fix qcfrequency

--- public/control/public_control.php
+++ public/control/public_control.php
@@ -93,25 +93,24 @@ saturne_header(0, '', $title);
 
 $qcFrequencyArray = [];
 $objectInfoArray  = [
-    'product'      => ['title' => 'Product',    'value' => img_picto('', 'product', 'class="pictofixedwidth"')],
-    'user'         => ['title' => 'User',       'value' => img_picto('', 'user', 'class="pictofixedwidth"')],
-    'societe'      => ['title' => 'ThirdParty', 'value' => img_picto('', 'company', 'class="pictofixedwidth"')],
-    'contact'      => ['title' => 'Contact',    'value' => img_picto('', 'contact', 'class="pictofixedwidth"')],
-    'project'      => ['title' => 'Project',    'value' => img_picto('', 'project', 'class="pictofixedwidth"')],
-    'project_task' => ['title' => 'Task',       'value' => img_picto('', 'projecttask', 'class="pictofixedwidth"')],
-    'facture'      => ['title' => 'Bill',       'value' => img_picto('', 'bill', 'class="pictofixedwidth"')],
-    'commande'     => ['title' => 'Order',      'value' => img_picto('', 'order', 'class="pictofixedwidth"')],
-    'contrat'      => ['title' => 'Contract',   'value' => img_picto('', 'contract', 'class="pictofixedwidth"')],
-    'ticket'       => ['title' => 'Ticket',     'value' => img_picto('', 'ticket', 'class="pictofixedwidth"')]
+    'product'      => ['title' => 'Product',    'picto' => img_picto('', 'product', 'class="pictofixedwidth"')],
+    'user'         => ['title' => 'User',       'picto' => img_picto('', 'user', 'class="pictofixedwidth"')],
+    'societe'      => ['title' => 'ThirdParty', 'picto' => img_picto('', 'company', 'class="pictofixedwidth"')],
+    'contact'      => ['title' => 'Contact',    'picto' => img_picto('', 'contact', 'class="pictofixedwidth"')],
+    'project'      => ['title' => 'Project',    'picto' => img_picto('', 'project', 'class="pictofixedwidth"')],
+    'project_task' => ['title' => 'Task',       'picto' => img_picto('', 'projecttask', 'class="pictofixedwidth"')],
+    'facture'      => ['title' => 'Bill',       'picto' => img_picto('', 'bill', 'class="pictofixedwidth"')],
+    'commande'     => ['title' => 'Order',      'picto' => img_picto('', 'order', 'class="pictofixedwidth"')],
+    'contrat'      => ['title' => 'Contract',   'picto' => img_picto('', 'contract', 'class="pictofixedwidth"')],
+    'ticket'       => ['title' => 'Ticket',     'picto' => img_picto('', 'ticket', 'class="pictofixedwidth"')]
 ];
 $object->fetchObjectLinked('', '', '', 'dolismq_control');
 foreach ($object->linkedObjectsIds as $key => $linkedObjects) {
     // Special case
     if ($key == 'productbatch') {
-        $productlot = new Productlot($db);
-        $productlot->fetch(array_shift($object->linkedObjectsIds['productbatch']));
-        $objectInfoArray[] = ['title' => 'Batch', 'value' => img_picto('', 'productlot', 'class="pictofixedwidth"') . $productlot->batch];
-        $qcFrequencyArray[$key] =  $productlot->array_options['options_qc_frequency'];
+        $linkedObject = new Productlot($db);
+        $linkedObject->fetch(array_shift($object->linkedObjectsIds['productbatch']));
+        $objectInfoArray[$key] = ['title' => 'Batch', 'picto' => img_picto('', 'lot', 'class="pictofixedwidth"'), 'value' => $linkedObject->batch];
     } elseif (!empty($object->linkedObjects[$key])) {
         $linkedObject = array_values($object->linkedObjects[$key])[0];
         switch ($key) {
@@ -126,18 +125,20 @@ foreach ($object->linkedObjectsIds as $key => $linkedObjects) {
                 $objectInfoArray[$key]['value'] .= $linkedObject->ref;
                 break;
         }
+    }
+    if (!empty($linkedObject->array_options['options_qc_frequency'])) {
         $qcFrequencyArray[$key] = $linkedObject->array_options['options_qc_frequency'];
     }
 }
 
 ?>
 
-<div class="signature-container">
+<div class="signature-container" style="max-width: 1000px;">
     <div class="wpeo-gridlayout grid-2">
         <div style="display: flex; justify-content: center; align-items: center;"><?php echo saturne_show_medias_linked('dolismq', $conf->dolismq->multidir_output[$conf->entity] . '/' . $object->element . '/'. $object->ref . '/photos/', 'small', '', 0, 0, 0, 200, 200, 0, 0, 0, $object->element . '/'. $object->ref . '/photos/', $object, 'photo', 0, 0,0, 1); ?></div>
         <div class="informations">
             <div style="margin-bottom: 10px"><strong><?php echo $object->getNomUrl(1, 'nolink'); ?></strong></div>
-            <div class="wpeo-table table-flex table-3">
+            <div class="wpeo-table table-flex">
                 <div class="table-row">
                     <div class="table-cell"><?php echo '<i class="far fa-check-circle"></i> ' . $langs->trans('Verdict'); ?></div>
                     <div class="table-cell table-end"><?php echo (!empty($object->verdict) ? $langs->transnoentities($object->fields['verdict']['arrayofkeyval'][$object->verdict]) : $langs->transnoentities('NoVerdict')); ?></div>
@@ -146,10 +147,10 @@ foreach ($object->linkedObjectsIds as $key => $linkedObjects) {
                     <div class="table-cell"><?php echo $langs->trans('ObjectLinked'); ?></div>
                         <div class="wpeo-table table-cell table-full">
                             <?php foreach ($objectInfoArray as $key => $linkedObject) : ?>
-                                <?php if (array_key_exists($key, $qcFrequencyArray)) : ?>
+                                <?php if (!empty($linkedObject['value'])) : ?>
                                     <div class="table-row">
-                                        <div class="table-cell table-100"><?php echo (($key == array_keys($qcFrequencyArray, min($qcFrequencyArray))[0]) ? '<strong>' . $langs->transnoentities($linkedObject['title']) . ' : ' . '</strong>' : $langs->transnoentities($linkedObject['title']) . ' : '); ?></div>
-                                        <div class="table-cell table-end"><?php echo (($key == array_keys($qcFrequencyArray, min($qcFrequencyArray))[0]) ? '<strong>' . $linkedObject['value'] . '</strong>' : $linkedObject['value']); ?></div>
+                                        <div class="table-cell table-150"><?php echo (($key == array_keys($qcFrequencyArray, min($qcFrequencyArray))[0]) ? '<strong>' . $langs->transnoentities($linkedObject['title']) . ' : ' . '</strong>' : $langs->transnoentities($linkedObject['title']) . ' : '); ?></div>
+                                        <div class="table-cell table-200 table-end"><?php echo (($key == array_keys($qcFrequencyArray, min($qcFrequencyArray))[0]) ? '<strong>' . $linkedObject['picto'] . $linkedObject['value'] . '</strong>' : $linkedObject['picto'] . $linkedObject['value']); ?></div>
                                     </div>
                                 <?php endif; ?>
                             <?php endforeach; ?>
@@ -167,7 +168,7 @@ foreach ($object->linkedObjectsIds as $key => $linkedObjects) {
                     </div>
                     <div class="table-row">
                         <div class="table-cell table-200"><?php echo img_picto('', $object->picto, 'class="pictofixedwidth"') . $langs->trans('NextControl'); ?></div>
-                        <div class="table-cell table-75 table-end badge badge-status <?php echo ((($nextControlDate - $object->date_creation) > 0) ? 'badge-status4' : 'badge-status8'); ?>"><?php echo floor(($nextControlDate - $object->date_creation)/(3600 * 24)) . ' ' . $langs->trans('Days'); ?></div>
+                        <div class="table-cell table-75 table-end badge badge-status <?php echo ((($nextControlDate - dol_now()) > 0) ? 'badge-status4' : 'badge-status8'); ?>"><?php echo floor(($nextControlDate - dol_now())/(3600 * 24)) . ' ' . $langs->trans('Days'); ?></div>
                     </div>
                 <?php endif; ?>
             </div>
