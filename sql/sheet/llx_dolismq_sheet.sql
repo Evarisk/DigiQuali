437d1fb110a9c97cb0f9946f2e9fd67a2b5d2a5e
evarisk-micka <mickael.evarisk@gmail.com>
2023-06-21 11:33:23 +0200

    #1222 [SQL] fix: set mandatory to text and init default value in create

--- sql/sheet/llx_dolismq_sheet.sql
+++ sql/sheet/llx_dolismq_sheet.sql
@@ -26,7 +26,7 @@ CREATE TABLE llx_dolismq_sheet(
 	label               varchar(255) NOT NULL,
     description         text,
     element_linked      text,
-    mandatory_questions varchar(255) DEFAULT '{}' NOT NULL,
+    mandatory_questions text,
 	fk_user_creat       integer NOT NULL,
 	fk_user_modif       integer
 ) ENGINE=innodb;
