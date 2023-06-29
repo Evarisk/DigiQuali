ab5d88d390282da46553fbf02a9050848ed40f3f
Théo David <theo.evarisk@gmail.com>
2023-06-29 10:31:38 +0200

    #1187 [Control] fix: next control date in public interface

--- public/control/public_control.php
+++ public/control/public_control.php
@@ -23,37 +23,37 @@
  */
 
 if (!defined('NOREQUIREUSER')) {
-    define('NOREQUIREUSER', '1');
+	define('NOREQUIREUSER', '1');
 }
 if (!defined('NOTOKENRENEWAL')) {
-    define('NOTOKENRENEWAL', '1');
+	define('NOTOKENRENEWAL', '1');
 }
 if (!defined('NOREQUIREMENU')) {
-    define('NOREQUIREMENU', 1);
+	define('NOREQUIREMENU', 1);
 }
 if (!defined('NOREQUIREHTML')) {
-    define('NOREQUIREHTML', '1');
+	define('NOREQUIREHTML', '1');
 }
 if (!defined('NOLOGIN')) {      // This means this output page does not require to be logged.
-    define('NOLOGIN', '1');
+	define('NOLOGIN', '1');
 }
 if (!defined('NOCSRFCHECK')) {  // We accept to go on this page from external website.
-    define('NOCSRFCHECK', '1');
+	define('NOCSRFCHECK', '1');
 }
 if (!defined('NOIPCHECK')) {    // Do not check IP defined into conf $dolibarr_main_restrict_ip.
-    define('NOIPCHECK', '1');
+	define('NOIPCHECK', '1');
 }
 if (!defined('NOBROWSERNOTIF')) {
-    define('NOBROWSERNOTIF', '1');
+	define('NOBROWSERNOTIF', '1');
 }
 
 // Load DoliSMQ environment.
 if (file_exists('../../dolismq.main.inc.php')) {
-    require_once __DIR__ . '/../../dolismq.main.inc.php';
+	require_once __DIR__ . '/../../dolismq.main.inc.php';
 } elseif (file_exists('../../../dolismq.main.inc.php')) {
-    require_once __DIR__ . '/../../../dolismq.main.inc.php';
+	require_once __DIR__ . '/../../../dolismq.main.inc.php';
 } else {
-    die('Include of dolismq main fails');
+	die('Include of dolismq main fails');
 }
 
 // Load Dolibarr libraries.
@@ -94,102 +94,91 @@ saturne_header(0, '', $title);
 
 $elementArray = get_sheet_linkable_objects();
 
-$qcFrequencyArray = [];
-$linkedObjects    = [];
-
 $object->fetchObjectLinked('', '', '', 'dolismq_control');
-
-foreach($elementArray as $linkableElementType => $linkableElement) {
-	if ($linkableElement['conf'] > 0 && (!empty($object->linkedObjectsIds[$linkableElement['link_name']]))) {
-
-	}
-}
 ?>
 
-<div class="signature-container" style="max-width: 1000px;">
-    <div class="wpeo-gridlayout grid-2">
-        <div style="display: flex; justify-content: center; align-items: center;"><?php echo saturne_show_medias_linked('dolismq', $conf->dolismq->multidir_output[$conf->entity] . '/' . $object->element . '/'. $object->ref . '/photos/', 'small', '', 0, 0, 0, 200, 200, 0, 0, 0, $object->element . '/'. $object->ref . '/photos/', $object, 'photo', 0, 0,0, 1); ?></div>
-        <div class="informations">
-            <div style="margin-bottom: 10px"><strong><?php echo $object->getNomUrl(1, 'nolink'); ?></strong></div>
-            <div class="wpeo-table table-flex">
-                <div class="table-row">
-                    <div class="table-cell"><?php echo '<i class="far fa-check-circle"></i> ' . $langs->trans('Verdict'); ?></div>
-                    <div class="table-cell table-end"><?php echo (!empty($object->verdict) ? $langs->transnoentities($object->fields['verdict']['arrayofkeyval'][$object->verdict]) : $langs->transnoentities('NoVerdict')); ?></div>
-                </div>
-				<div class="table-row">
-					<div class="wpeo-table table-cell table-full">
-						<?php
-						foreach ($elementArray as $linkableObjectType => $linkableObject) {
-							if ($linkableObject['conf'] > 0 && (!empty($object->linkedObjectsIds[$linkableObject['link_name']]))) {
+	<div class="signature-container" style="max-width: 1000px;">
+		<div class="wpeo-gridlayout grid-2">
+			<div style="display: flex; justify-content: center; align-items: center;"><?php echo saturne_show_medias_linked('dolismq', $conf->dolismq->multidir_output[$conf->entity] . '/' . $object->element . '/'. $object->ref . '/photos/', 'small', '', 0, 0, 0, 200, 200, 0, 0, 0, $object->element . '/'. $object->ref . '/photos/', $object, 'photo', 0, 0,0, 1); ?></div>
+			<div class="informations">
+				<div style="margin-bottom: 10px"><strong><?php echo $object->getNomUrl(1, 'nolink'); ?></strong></div>
+				<div class="wpeo-table table-flex">
+					<div class="table-row">
+						<div class="table-cell"><?php echo '<i class="far fa-check-circle"></i> ' . $langs->trans('Verdict'); ?></div>
+						<div class="table-cell table-end"><?php echo (!empty($object->verdict) ? $langs->transnoentities($object->fields['verdict']['arrayofkeyval'][$object->verdict]) : $langs->transnoentities('NoVerdict')); ?></div>
+					</div>
+					<div class="table-row">
+						<div class="wpeo-table table-cell table-full">
+							<?php
+							foreach ($elementArray as $linkableObjectType => $linkableObject) {
+								if ($linkableObject['conf'] > 0 && (!empty($object->linkedObjectsIds[$linkableObject['link_name']]))) {
 
-								$className    = $linkableObject['className'];
-								$linkedObject = new $className($db);
+									$className    = $linkableObject['className'];
+									$linkedObject = new $className($db);
 
-								$linkedObjectKey = array_key_first($object->linkedObjectsIds[$linkableObject['link_name']]);
-								$linkedObjectId  = $object->linkedObjectsIds[$linkableObject['link_name']][$linkedObjectKey];
+									$linkedObjectKey = array_key_first($object->linkedObjectsIds[$linkableObject['link_name']]);
+									$linkedObjectId  = $object->linkedObjectsIds[$linkableObject['link_name']][$linkedObjectKey];
 
 
-								$result = $linkedObject->fetch($linkedObjectId);
-								if ($result > 0) {
-									$linkedObject->fetch_optionals();
+									$result = $linkedObject->fetch($linkedObjectId);
+									if ($result > 0) {
+										$linkedObject->fetch_optionals();
 
-									$objectName = '';
-									$objectNameField = $linkableObject['name_field'];
+										$objectName = '';
+										$objectNameField = $linkableObject['name_field'];
 
-									if (strstr($objectNameField, ',')) {
-										$nameFields = explode(', ', $objectNameField);
-										if (is_array($nameFields) && !empty($nameFields)) {
-											foreach ($nameFields as $subnameField) {
-												$objectName .= $linkedObject->$subnameField . ' ';
+										if (strstr($objectNameField, ',')) {
+											$nameFields = explode(', ', $objectNameField);
+											if (is_array($nameFields) && !empty($nameFields)) {
+												foreach ($nameFields as $subnameField) {
+													$objectName .= $linkedObject->$subnameField . ' ';
+												}
 											}
+										} else {
+											$objectName = $linkedObject->$objectNameField;
 										}
-									} else {
-										$objectName = $linkedObject->$objectNameField;
+
+										print '<div class="table-row">';
+										print '<div class="table-cell table-150">';
+										print '<strong>';
+										print $langs->transnoentities($linkableObject['langs']);
+										print '</div>';
+										print '<div class="table-cell table-end">';
+										print $objectName . ' ' . img_picto('', $linkableObject['picto'], 'class="pictofixedwidth"');
+
+										if ($linkedObject->array_options['options_qc_frequency'] > 0) {
+											$objectQcFrequency = $linkedObject->array_options['options_qc_frequency'];
+											print '<br>';
+											print $langs->transnoentities('QcFrequency') . ' : ' . $objectQcFrequency;
+										}
+										print '</strong>';
+										print '</div>';
+										print '</div>';
 									}
-
-									print '<div class="table-row">';
-									print '<div class="table-cell table-150">';
-
-									print '<strong>';
-									print $langs->transnoentities($linkableObject['langs']);
-									print '</div>';
-									print '<div class="table-cell table-end">';
-									print $objectName . ' ' . img_picto('', $linkableObject['picto'], 'class="pictofixedwidth"');
-
-									if ($linkedObject->array_options['options_qc_frequency'] > 0) {
-										print '<br>';
-										print $langs->transnoentities('QcFrequency') . ' : ' . $linkedObject->array_options['options_qc_frequency'];
-									}
-
-									print '</strong>';
-
-									print '</div>';
-									print '</div>';
 								}
 							}
-						}
-						?>
+							?>
+						</div>
 					</div>
+					<div class="table-row">
+						<div class="table-cell table-200"><?php echo img_picto('', 'calendar', 'class="pictofixedwidth"') . $langs->trans('ControlDate'); ?></div>
+						<div class="table-cell table-end"><?php echo dol_print_date($object->date_creation, 'day'); ?></div>
+					</div>
+					<?php if (!empty($objectQcFrequency)) : ?>
+						<div class="table-row">
+							<div class="table-cell table-300"><?php echo img_picto('', 'calendar', 'class="pictofixedwidth"') . $langs->trans('NextControlDate'); ?></div>
+							<?php $nextControlDate = dol_time_plus_duree($object->date_creation, $objectQcFrequency, 'd'); ?>
+							<div class="table-cell table-end"><?php echo dol_print_date($nextControlDate, 'day'); ?></div>
+						</div>
+						<div class="table-row">
+							<div class="table-cell table-200"><?php echo img_picto('', $object->picto, 'class="pictofixedwidth"') . $langs->trans('NextControl'); ?></div>
+							<div class="table-cell table-75 table-end badge badge-status <?php echo ((($nextControlDate - dol_now()) > 0) ? 'badge-status4' : 'badge-status8'); ?>"><?php echo floor(($nextControlDate - dol_now())/(3600 * 24)) . ' ' . $langs->trans('Days'); ?></div>
+						</div>
+					<?php endif; ?>
 				</div>
-                <div class="table-row">
-                    <div class="table-cell table-200"><?php echo img_picto('', 'calendar', 'class="pictofixedwidth"') . $langs->trans('ControlDate'); ?></div>
-                    <div class="table-cell table-end"><?php echo dol_print_date($object->date_creation, 'day'); ?></div>
-                </div>
-                <?php if (!empty($qcFrequencyArray)) : ?>
-                    <div class="table-row">
-                        <div class="table-cell table-300"><?php echo img_picto('', 'calendar', 'class="pictofixedwidth"') . $langs->trans('NextControlDate'); ?></div>
-                        <?php $nextControlDate = dol_time_plus_duree($object->date_creation, min($qcFrequencyArray), 'd'); ?>
-                        <div class="table-cell table-end"><?php echo dol_print_date($nextControlDate, 'day'); ?></div>
-                    </div>
-                    <div class="table-row">
-                        <div class="table-cell table-200"><?php echo img_picto('', $object->picto, 'class="pictofixedwidth"') . $langs->trans('NextControl'); ?></div>
-                        <div class="table-cell table-75 table-end badge badge-status <?php echo ((($nextControlDate - dol_now()) > 0) ? 'badge-status4' : 'badge-status8'); ?>"><?php echo floor(($nextControlDate - dol_now())/(3600 * 24)) . ' ' . $langs->trans('Days'); ?></div>
-                    </div>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</div>
+			</div>
+		</div>
+	</div>
 
 <?php llxFooter('', 'public');
 $db->close();
