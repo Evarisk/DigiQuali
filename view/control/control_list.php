4247098e53e5d3a1d7f6eda29ca6b23b9c72df2e
Nicolas Domenech <nicolas@eoxia.com>
2023-12-08 16:01:16 +0100

    #1599 [Control] fix: remove unused fields and fix banner tab
    add navigation link

--- view/control/control_list.php
+++ view/control/control_list.php
@@ -127,16 +127,15 @@ foreach($linkableElements as $linkableElementType => $linkableElement) {
 	if (!empty($fromtype) && $fromtype == $linkableElement['link_name']) {
 		$objectLinked = new $className($db);
 		$objectLinked->fetch($fromid);
+        $arrayfields['t.'.$linkableElement['post_name']] = [
+            'type' => 'integer:'. $className .':' . $linkableElement['class_path'], 'label' => $langs->trans($linkableElement['langs']) . ' ' . $langs->trans('controlled') , 'enabled' => '1', 'position' => $objectPosition, 'notnull' => 0, 'visible' => 5, 'checked' => 1
+        ];
+
+        $object->fields[$linkableElement['post_name']] = $arrayfields['t.'.$linkableElement['post_name']];
+        $elementElementFields[$linkableElement['post_name']] = $linkableElement['link_name'];
+        $linkNameElementCorrespondance[$linkableElement['link_name']] = $linkableElement;
+        $objectPosition++;
 	}
-
-	$arrayfields['t.'.$linkableElement['post_name']] = [
-		'type' => 'integer:'. $className .':' . $linkableElement['class_path'], 'label' => $langs->trans($linkableElement['langs']) . ' ' . $langs->trans('controlled') , 'enabled' => '1', 'position' => $objectPosition, 'notnull' => 0, 'visible' => 5, 'checked' => 1
-	];
-
-	$object->fields[$linkableElement['post_name']] = $arrayfields['t.'.$linkableElement['post_name']];
-	$elementElementFields[$linkableElement['post_name']] = $linkableElement['link_name'];
-	$linkNameElementCorrespondance[$linkableElement['link_name']] = $linkableElement;
-	$objectPosition++;
 }
 
 // Initialize array of search criterias
@@ -297,7 +296,7 @@ if (!empty($fromtype)) {
 
 	$linkback = '<a href="'.DOL_URL_ROOT.'/'.$fromtype.'/list.php?restore_lastsearch_values=1">'.$langs->trans("BackToList").'</a>';
 
-	saturne_banner_tab($objectLinked, 'ref', '', 0);
+	saturne_banner_tab($objectLinked, 'fromtype=' . $fromtype . '&fromid', '', 1, 'rowid', ($fromtype == 'productbatch' ? 'batch' : 'ref'));
 }
 
 if ($fromid) {
