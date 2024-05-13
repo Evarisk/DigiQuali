a2bd7f1eec06d538e5b9e3cc1d2d7830c3ce2b53
Charles Delachapelle <charles.delachapelle@evarisk.com>
2024-05-13 09:22:13 +0200

    #1788 [Question] add: description field on question list

--- class/question.class.php
+++ class/question.class.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2022-2023 EVARISK <technique@evarisk.com>
+/* Copyright (C) 2022-2024 EVARISK <technique@evarisk.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 /**
  * \file    class/question.class.php
  * \ingroup digiquali
- * \brief   This file is a CRUD class file for Question (Create/Read/Update/Delete).
+ * \brief   This file is a CRUD class file for Question (Create/Read/Update/Delete)
  */
 
 // Load Saturne libraries.
@@ -30,33 +30,33 @@ require_once __DIR__ . '/../../saturne/class/saturneobject.class.php';
 class Question extends SaturneObject
 {
     /**
-     * @var string Module name.
+     * @var string Module name
      */
     public $module = 'digiquali';
 
     /**
-     * @var string Element type of object.
+     * @var string Element type of object
      */
     public $element = 'question';
 
     /**
-     * @var string Name of table without prefix where object is stored. This is also the key used for extrafields management.
+     * @var string Name of table without prefix where object is stored. This is also the key used for extrafields management
      */
     public $table_element = 'digiquali_question';
 
     /**
      * @var int Does this object support multicompany module ?
-     * 0 = No test on entity, 1 = Test with field entity, 'field@table' = Test with link by field@table.
+     * 0 = No test on entity, 1 = Test with field entity, 'field@table' = Test with link by field@table
      */
     public $ismultientitymanaged = 1;
 
     /**
-     * @var int Does object support extrafields ? 0 = No, 1 = Yes.
+     * @var int Does object support extrafields ? 0 = No, 1 = Yes
      */
     public $isextrafieldmanaged = 1;
 
     /**
-     * @var string Name of icon for control. Must be a 'fa-xxx' fontawesome code (or 'fa-xxx_fa_color_size') or 'control@digiquali' if picto is file 'img/object_control.png'.
+     * @var string Name of icon for control. Must be a 'fa-xxx' fontawesome code (or 'fa-xxx_fa_color_size') or 'control@digiquali' if picto is file 'img/object_control.png'
      */
     public string $picto = 'fontawesome_fa-question_fas_#d35968';
 
@@ -79,33 +79,33 @@ class Question extends SaturneObject
      *      'boolean', 'checkbox', 'radio', 'array',
      *      'mail', 'phone', 'url', 'password', 'ip'
      *      Note: Filter can be a string like "(t.ref:like:'SO-%') or (t.date_creation:<:'20160101') or (t.nature:is:NULL)"
-     * 'label' the translation key.
+     * 'label' the translation key
      * 'picto' is code of a picto to show before value in forms
      * 'enabled' is a condition when the field must be managed (Example: 1 or '$conf->global->MY_SETUP_PARAM' or '!empty($conf->multicurrency->enabled)' ...)
-     * 'position' is the sort order of field.
-     * 'notnull' is set to 1 if not null in database. Set to -1 if we must set data to null if empty '' or 0.
+     * 'position' is the sort order of field
+     * 'notnull' is set to 1 if not null in database. Set to -1 if we must set data to null if empty '' or 0
      * 'visible' says if field is visible in list (Examples: 0=Not visible, 1=Visible on list and create/update/view forms, 2=Visible on list only, 3=Visible on create/update/view form only (not list), 4=Visible on list and update/view form only (not create). 5=Visible on list and view only (not create/not update). Using a negative value means field is not shown by default on list but can be selected for viewing)
      * 'noteditable' says if field is not editable (1 or 0)
-     * 'default' is a default value for creation (can still be overwroted by the Setup of Default Values if field is editable in creation form). Note: If default is set to '(PROV)' and field is 'ref', the default value will be set to '(PROVid)' where id is rowid when a new record is created.
-     * 'index' if we want an index in database.
-     * 'foreignkey'=>'tablename.field' if the field is a foreign key (it is recommanded to name the field fk_...).
-     * 'searchall' is 1 if we want to search in this field when making a search from the quick search button.
+     * 'default' is a default value for creation (can still be overwroted by the Setup of Default Values if field is editable in creation form). Note: If default is set to '(PROV)' and field is 'ref', the default value will be set to '(PROVid)' where id is rowid when a new record is created
+     * 'index' if we want an index in database
+     * 'foreignkey'=>'tablename.field' if the field is a foreign key (it is recommanded to name the field fk_...)
+     * 'searchall' is 1 if we want to search in this field when making a search from the quick search button
      * 'isameasure' must be set to 1 or 2 if field can be used for measure. Field type must be summable like integer or double(24,8). Use 1 in most cases, or 2 if you don't want to see the column total into list (for example for percentage)
      * 'css' and 'cssview' and 'csslist' is the CSS style to use on field. 'css' is used in creation and update. 'cssview' is used in view mode. 'csslist' is used for columns in lists. For example: 'css'=>'minwidth300 maxwidth500 widthcentpercentminusx', 'cssview'=>'wordbreak', 'csslist'=>'tdoverflowmax200'
-     * 'help' is a 'TranslationString' to use to show a tooltip on field. You can also use 'TranslationString:keyfortooltiponlick' for a tooltip on click.
+     * 'help' is a 'TranslationString' to use to show a tooltip on field. You can also use 'TranslationString:keyfortooltiponlick' for a tooltip on click
      * 'showoncombobox' if value of the field must be visible into the label of the combobox that list record
-     * 'disabled' is 1 if we want to have the field locked by a 'disabled' attribute. In most cases, this is never set into the definition of $fields into class, but is set dynamically by some part of code.
+     * 'disabled' is 1 if we want to have the field locked by a 'disabled' attribute. In most cases, this is never set into the definition of $fields into class, but is set dynamically by some part of code
      * 'arrayofkeyval' to set a list of values if type is a list of predefined values. For example: array("0"=>"Draft","1"=>"Active","-1"=>"Cancel"). Note that type can be 'integer' or 'varchar'
-     * 'autofocusoncreate' to have field having the focus on a create form. Only 1 field should have this property set to 1.
-     * 'comment' is not used. You can store here any text of your choice. It is not used by application.
+     * 'autofocusoncreate' to have field having the focus on a create form. Only 1 field should have this property set to 1
+     * 'comment' is not used. You can store here any text of your choice. It is not used by application
      * 'validate' is 1 if you need to validate with $this->validateField()
      * 'copytoclipboard' is 1 or 2 to allow to add a picto to copy value into clipboard (1=picto after label, 2=picto after value)
      *
-     * Note: To have value dynamic, you can set value to 0 in definition and edit the value on the fly into the constructor.
+     * Note: To have value dynamic, you can set value to 0 in definition and edit the value on the fly into the constructor
      */
 
     /**
-     * @var array Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
+     * @var array Array with all fields and their property. Do not use it as a static var. It may be modified by constructor
      */
 	public $fields = [
         'rowid'                  => ['type' => 'integer',      'label' => 'TechnicalID',          'enabled' => 1, 'position' => 1,   'notnull' => 1, 'visible' => 0, 'noteditable' => 1, 'index' => 1, 'comment' => 'Id'],
@@ -118,7 +118,7 @@ class Question extends SaturneObject
         'status'                 => ['type' => 'smallint',     'label' => 'Status',               'enabled' => 1, 'position' => 70,  'notnull' => 1, 'visible' => 5, 'index' => 1, 'default' => 0, 'arrayofkeyval' => ['specialCase' => 'InProgressAndLocked', 1 => 'InProgress', 2 => 'Locked', 3 => 'Archived'], 'css' => 'minwidth200'],
 		'type'                   => ['type' => 'varchar(128)', 'label' => 'Type',                 'enabled' => 1, 'position' => 80,  'notnull' => 1, 'visible' => 1],
 		'label'                  => ['type' => 'varchar(255)', 'label' => 'Label',                'enabled' => 1, 'position' => 11,  'notnull' => 1, 'visible' => 1, 'searchall' => 1, 'css' => 'minwidth200', 'showoncombobox' => 1],
-		'description'            => ['type' => 'html',         'label' => 'Description',          'enabled' => 1, 'position' => 100, 'notnull' => 0, 'visible' => 3],
+		'description'            => ['type' => 'html',         'label' => 'Description',          'enabled' => 1, 'position' => 100, 'notnull' => 0, 'visible' => 1],
 		'show_photo'             => ['type' => 'boolean',      'label' => 'ShowPhoto',            'enabled' => 1, 'position' => 110, 'notnull' => 0, 'visible' => 0],
 		'authorize_answer_photo' => ['type' => 'boolean',      'label' => 'AuthorizeAnswerPhoto', 'enabled' => 1, 'position' => 120, 'notnull' => 0, 'visible' => 0],
 		'enter_comment'          => ['type' => 'boolean',      'label' => 'EnterComment',         'enabled' => 1, 'position' => 130, 'notnull' => 0, 'visible' => 0],
@@ -129,99 +129,99 @@ class Question extends SaturneObject
     ];
 
     /**
-     * @var int ID.
+     * @var int ID
      */
     public int $rowid;
 
     /**
-     * @var string Ref.
+     * @var string Ref
      */
     public $ref;
 
     /**
-     * @var string Ref ext.
+     * @var string Ref ext
      */
     public $ref_ext;
 
     /**
-     * @var int Entity.
+     * @var int Entity
      */
     public $entity;
 
     /**
-     * @var int|string Creation date.
+     * @var int|string Creation date
      */
     public $date_creation;
 
     /**
-     * @var int|string Timestamp.
+     * @var int|string Timestamp
      */
     public $tms;
 
     /**
-     * @var string Import key.
+     * @var string Import key
      */
     public $import_key;
 
     /**
-     * @var int Status.
+     * @var int Status
      */
     public $status;
 
     /**
-     * @var string Type.
+     * @var string Type
      */
     public string $type;
 
     /**
-     * @var string Label.
+     * @var string Label
      */
     public string $label;
 
     /**
-     * @var string|null Description.
+     * @var string|null Description
      */
     public ?string $description;
 
     /**
-     * @var bool|null Show photo.
+     * @var bool|null Show photo
      */
     public ?bool $show_photo = null;
 
     /**
-     * @var bool|null Authorize answer photo.
+     * @var bool|null Authorize answer photo
      */
     public ?bool $authorize_answer_photo = null;
 
     /**
-     * @var bool|null Comment.
+     * @var bool|null Comment
      */
     public ?bool $enter_comment = null;
 
     /**
-     * @var string|null Photo OK.
+     * @var string|null Photo OK
      */
     public ?string $photo_ok = '';
 
     /**
-     * @var string|null Photo KO.
+     * @var string|null Photo KO
      */
     public ?string $photo_ko = '';
 
     /**
-     * @var int User ID.
+     * @var int User ID
      */
     public $fk_user_creat;
 
     /**
-     * @var int|null User ID.
+     * @var int|null User ID
      */
     public $fk_user_modif;
 
     /**
-     * Constructor.
+     * Constructor
      *
-     * @param DoliDb $db Database handler.
+     * @param DoliDb $db Database handler
      */
     public function __construct(DoliDB $db)
     {
@@ -229,11 +229,11 @@ class Question extends SaturneObject
     }
 
     /**
-     * Create object into database.
+     * Create object into database
      *
-     * @param  User $user      User that creates.
-     * @param  bool $notrigger false = launch triggers after, true = disable triggers.
-     * @return int             0 < if KO, ID of created object if OK.
+     * @param  User $user      User that creates
+     * @param  bool $notrigger false = launch triggers after, true = disable triggers
+     * @return int             0 < if KO, ID of created object if OK
      */
     public function create(User $user, bool $notrigger = false): int
     {
@@ -289,11 +289,11 @@ class Question extends SaturneObject
 	}
 
     /**
-     * Return the status.
+     * Return the status
      *
-     * @param  int    $status ID status.
-     * @param  int    $mode   0 = long label, 1 = short label, 2 = Picto + short label, 3 = Picto, 4 = Picto + long label, 5 = Short label + Picto, 6 = Long label + Picto.
-     * @return string         Label of status.
+     * @param  int    $status ID status
+     * @param  int    $mode   0 = long label, 1 = shot label, 2 = Picto + short label, 3 = Picto, 4 = Picto + long label, 5 = Short label + Picto, 6 = Long label + Picto
+     * @return string         Label of status
      */
     public function LibStatut(int $status, int $mode = 0): string
     {
@@ -332,7 +332,7 @@ class Question extends SaturneObject
 	 * @param  User      $user    User that creates
 	 * @param  int       $fromid  ID of object to clone
 	 * @param  array     $options Options array
-	 * @return int                New object created, <0 if KO
+	 * @return int                New object created, < 0 if KO
 	 * @throws Exception
 	 */
 	public function createFromClone(User $user, int $fromid, array $options): int
@@ -470,8 +470,8 @@ class Question extends SaturneObject
 	}
 
 	/**
-	 *  Output html form to select a third party.
-	 *  Note, you must use the select_company to get the component to select a third party. This function must only be called by select_company.
+	 *  Output html form to select a third party
+	 *  Note, you must use the select_company to get the component to select a third party. This function must only be called by select_company
 	 *
 	 * @param string $selected   Preselected type
 	 * @param string $htmlname   Name of field in form
