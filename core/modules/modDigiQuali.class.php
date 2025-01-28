cac0048cc55ac0880fb234dcbdfde9a8a4f552d4
Nicolas Domenech <nicolas@eoxia.com>
2025-01-28 10:12:38 +0100

    #1971 [Mod] add: version number 20.0.0

--- core/modules/modDigiQuali.class.php
+++ core/modules/modDigiQuali.class.php
@@ -77,7 +77,7 @@ class modDigiQuali extends DolibarrModules
 		$this->editor_url = 'https://evarisk.com/';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.13.0';
+		$this->version = '20.0.0';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
@@ -161,7 +161,7 @@ class modDigiQuali extends DolibarrModules
 
 		// Prerequisites
 		$this->phpmin = [7, 4]; // Minimum version of PHP required by module
-		$this->need_dolibarr_version = [17, 0]; // Minimum version of Dolibarr required by module
+		$this->need_dolibarr_version = [20, 0]; // Minimum version of Dolibarr required by module
 
 		// Messages at activation
 		$this->warnings_activation = []; // Warning to show when we activate module. array('always'='text') or array('FR'='textfr','ES'='textes'...)
